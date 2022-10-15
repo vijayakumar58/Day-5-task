7b6b48c10ec2260903c9939173824e718692c3b9
vijayakumar58 <vijayalingam58@gmail.com>
2022-10-15 20:44:11 +0530

    add js

--- script.js
+++ script.js
@@ -3,13 +3,20 @@
 
 let arr=[1,2,3,34,54,55,34,32,11,19,17,54]
 let odd=[];
-let nu=function(arr){
-    for(var i=0;i<arr.length;i++){
-        if(arr[i]%2==1){
+let num = function(arr){
+    for(i=0;i<arr.length;i++){
+         if(arr[i]%2==1){
             odd.push(arr[i])
-        }else{
-
-        }
+         }
     }
+          return odd;
 }
-console.log(odd);
+console.log(num(arr)); 
+
+//b.Convert all the strings to title caps in a string array
+
+let txt = ["geeks for geek"]
+let str = function (){
+      return (txt.charAat(0).toUpperCase()+txt.substr(1).toLowerCase());
+    }
+    console.log(str(txt));
