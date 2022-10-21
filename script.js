d76bfc12a0fefdf0503312447632febbcbae44d8
vijayakumar58 <vijayalingam58@gmail.com>
2022-10-21 20:25:59 +0530

    add js

--- script.js
+++ script.js
@@ -27,11 +27,77 @@ return title.replace(title[0],title[0].toUpperCase());
   //c.Sum of all numbers in an array
   
   let arr1=[11,12,13,14,15,16,17,18,19,20];
-  function res(a,b,...rest){
+     var res=function (arr1){
       var sum=0;
       for(var i=0;i<arr1.length;i++){
-            sum=sum+res[i];
+            sum=sum+arr1[i];
       }
          return sum;
   }
-  console.log(res(...arr1));
+  console.log(res(arr1));
+
+  //d.Return all the prime numbers in an array
+
+  let arr2=[11,12,13,14,15,16,17,18,19,20];
+   var prime=function(arr2){
+      var primeNumber=[];
+       arr2.forEach((element) => {
+            let factor=0;
+            for (let i=0;i<=element;i++){
+                  if (element%i ===0){
+                        factor++
+                  }
+            }
+            if(factor===2){
+                  primeNumber.push(element)
+            }
+       }) 
+       return primeNumber;
+      }
+      console.log(prime(arr2));
+
+//e.Return all the palindromes in an array
+
+let arr3=["madam","apple","appa","google"];
+  let palin=[];
+var pal=function(arr3){
+      for(var i=0;i<arr3.length;i++){
+           palin.push( arr3[i].split("").reverse().join(""))
+      }
+      return palin;
+}
+console.log(pal(arr3));
+
+//f.Return median of two sorted arrays of the same size.
+
+var arr4=[1,4,7,3];
+var arr5=[12,19,15,18];
+var ord=[...arr4.sort(),...arr5.sort()];
+    let median=function(ord){
+      var ord1=ord.length/2;
+      let ord2=(ord[ord1]+ord[ord1+1])/2;
+              return ord2
+}
+console.log(median(ord));
+
+//g.Remove duplicates from an array
+
+    var arr6=["apple","orange","mango","painapple","orange","lemon","mango"]
+    let dup=function(arr6){
+       var set=new Set(arr6);
+       var arr7=[...set];
+       return arr7;
+    }
+    console.log(dup(arr6));
+
+//h.Rotate an array by k times
+
+var arr8=["apple","orange","mango","painapple","lemon"];
+  var k=2;
+      let rot=function(arr8){
+            for( var i=0;i<=k;i++){
+           var or=arr8.pop();arr8.unshift(or);
+            }
+              return arr8;
+}
+console.log(rot(arr8));
